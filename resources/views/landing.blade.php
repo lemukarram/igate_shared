56af61f6d98ec56af708e01031df216f0e15cf2b
Mukarram <mshah@rsg.one>
2026-04-30 20:02:18 +0300

    seeder run on server

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>iGate Shared Services - Standardized B2B Service Marketplace in KSA</title>
+    <title>iGate Shared Services - Standardized B2B Service Marketplace in KSA.</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest"></script>
     <style>
